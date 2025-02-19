492a3e235e412e394b15069f8a95bd0c33354f69
S3B4S <j.sn43@hotmail.com>
2025-02-18 20:44:31 -0500

    Adicion de la tabla del 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,9 +6,16 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("TABLAS DE MULTIPLICAR");
+        System.out.println("Tabla del 2");
         for (int i = 1; i <= 10; i++) {
 
             System.out.println("2 x " + i + " = " + 2 * i);
+        }
+
+        System.out.println("Tabla del 3");
+        for (int i = 1; i <= 10; i++) {
+
+            System.out.println("3 x " + i + " = " + 3 * i);
         }
 
     }
